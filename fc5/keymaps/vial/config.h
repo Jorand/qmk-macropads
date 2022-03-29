d17bb82bc58c5365dcbf5a30053d9e6b2bb0895a
Jorand <contact@jorand.fr>
2022-03-30 00:38:38 +0200

    add fc5 piano sustain pedal

---
+++ fc5/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA7, 0xDC, 0x6D, 0x86, 0x5F, 0x05, 0x6F, 0xE3}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
